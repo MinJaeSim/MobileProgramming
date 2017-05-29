bf17ac0d7d42395d7be8bf1b0bcd01cd66a2d31a
MinjaeSim <mj9757@naver.com>
2017-05-29 20:43:58 +0900

    그리드뷰 구현

--- app/src/main/java/com/example/yellow7918/mobile_sns/UI/MyPageFragment.java
+++ app/src/main/java/com/example/yellow7918/mobile_sns/UI/MyPageFragment.java
@@ -1,6 +1,10 @@
 package com.example.yellow7918.mobile_sns.UI;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,8 +12,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.yellow7918.mobile_sns.Model.Article;
 import com.example.yellow7918.mobile_sns.Model.UpLoader;
 import com.example.yellow7918.mobile_sns.Model.User;
@@ -22,7 +30,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyPageFragment extends Fragment {
+
+    List<String> uriList = new ArrayList();
 
     @Nullable
     @Override
@@ -41,9 +54,8 @@ public class MyPageFragment extends Fragment {
         myID.setTypeface(type);
 
         TextView myProfile = (TextView) v.findViewById(R.id.my_profile);
-        myProfile.setText(user.getProfile());
+        //myProfile.setText(user.getProfile());
         myProfile.setTypeface(type);
-
 
         final TextView myTextNum = (TextView) v.findViewById(R.id.my_text_number_view);
 
@@ -58,6 +70,7 @@ public class MyPageFragment extends Fragment {
                     Article article = e.getValue(Article.class);
                     if (article.getName().equals(user.getName())) {
                         textNumber++;
+                        uriList.add(article.getImageURL());
                     }
                 }
                 myTextNum.setText(textNumber + "개");
@@ -69,6 +82,51 @@ public class MyPageFragment extends Fragment {
             }
         });
 
+        GridView gridViewImages = (GridView) v.findViewById(R.id.gridViewImages);
+        ImageAdapter imageGridAdapter = new ImageAdapter(this.getContext(), uriList);
+        gridViewImages.setAdapter(imageGridAdapter);
+
+
         return v;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        Context context = null;
+
+        List<String> imageIDs = null;
+
+        public ImageAdapter(Context context, List imageIDs) {
+            this.context = context;
+            this.imageIDs = imageIDs;
+        }
+
+        public int getCount() {
+            return (null != imageIDs) ? imageIDs.size() : 0;
+        }
+
+        public Object getItem(int position) {
+            return (null != imageIDs) ? imageIDs.get(position) : 0;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = null;
+
+            if (null != convertView)
+                imageView = (ImageView) convertView;
+            else {
+                imageView = new ImageView(context);
+                imageView.setAdjustViewBounds(true);
+
+                Glide.with(MyPageFragment.this)
+                        .load(imageIDs.get(position))
+                        .into(imageView);
+
+            }
+            return imageView;
+        }
     }
 }
--- app/src/main/java/com/example/yellow7918/mobile_sns/UI/UserInfoFragment.java
+++ app/src/main/java/com/example/yellow7918/mobile_sns/UI/UserInfoFragment.java
@@ -1,5 +1,6 @@
 package com.example.yellow7918.mobile_sns.UI;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,8 +9,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.yellow7918.mobile_sns.Model.Article;
 import com.example.yellow7918.mobile_sns.Model.UpLoader;
 import com.example.yellow7918.mobile_sns.Model.User;
@@ -21,11 +26,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInfoFragment extends Fragment {
     private static final String ARG_USER_ID = "user_id";
 
     private int textNumber;
     private User mUser;
+    private List<String> uriList = new ArrayList<>();
 
     public static UserInfoFragment newInstance(String name) {
         Bundle args = new Bundle();
@@ -62,7 +71,7 @@ public class UserInfoFragment extends Fragment {
         mIdTV.setTypeface(type);
 
         TextView mProfileTV = (TextView) v.findViewById(R.id.user_profile);
-        mProfileTV.setText(mUser.getProfile());
+        //mProfileTV.setText(mUser.getProfile());
 
         final TextView mNumTextTV = (TextView) v.findViewById(R.id.user_text_number_view);
 
@@ -77,6 +86,7 @@ public class UserInfoFragment extends Fragment {
                     Article article = e.getValue(Article.class);
                     if (article.getName().equals(mUser.getName())) {
                         textNumber++;
+                        uriList.add(article.getImageURL());
                     }
                 }
                 mNumTextTV.setText(textNumber + "개");
@@ -88,6 +98,51 @@ public class UserInfoFragment extends Fragment {
             }
         });
 
+        GridView gridViewImages = (GridView) v.findViewById(R.id.user_gridViewImages);
+        UserInfoFragment.ImageAdapter imageGridAdapter = new UserInfoFragment.ImageAdapter(this.getContext(), uriList);
+        gridViewImages.setAdapter(imageGridAdapter);
+
         return v;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        Context context = null;
+
+        List<String> imageIDs = null;
+
+        public ImageAdapter(Context context, List imageIDs) {
+            this.context = context;
+            this.imageIDs = imageIDs;
+        }
+
+        public int getCount() {
+            return (null != imageIDs) ? imageIDs.size() : 0;
+        }
+
+        public Object getItem(int position) {
+            return (null != imageIDs) ? imageIDs.get(position) : 0;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = null;
+
+            if (null != convertView)
+                imageView = (ImageView) convertView;
+            else {
+
+                imageView = new ImageView(context);
+                imageView.setAdjustViewBounds(true);
+
+                Glide.with(UserInfoFragment.this)
+                        .load(imageIDs.get(position))
+                        .into(imageView);
+
+            }
+            return imageView;
+        }
     }
 }
